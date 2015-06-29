47598373699dc0b78d2764133c27c49ddd358b0e
kunal-das <kunaldasece@gmail.com>
2015-06-29 20:06:35 +0530

    final code of sort and search

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -11,13 +11,20 @@
 
 #include "helpers.h"
 
+int binarySearch(int A[], int l, int r, int x);
 /**
  * Returns true if value is in array of n values, else false.
  */
 bool search(int value, int values[], int n)
 {
     // TODO: implement a searching algorithm
-    return false;
+	int index = binarySearch(values, 0, n-1, value);
+		
+	if(index != -1)
+		return true;
+	else
+		return false;
+	
 }
 
 /**
@@ -25,6 +32,36 @@ bool search(int value, int values[], int n)
  */
 void sort(int values[], int n)
 {
-    // TODO: implement an O(n^2) sorting algorithm
-    return;
+    // TODO: implement an O(n^2) sorting algorithm. Implementing INSERION SORT.
+	int j;
+	int temp;
+	for(int i = 1; i < n; i++)
+	{
+	 	j=i;
+	 	while(j > 0 && values[j-1] > values[j])
+	 	{
+	 		temp = values[j-1];
+	 		values[j-1] = values[j];
+	 		values[j] = temp;
+	 		j--;
+	 	}
+	} 
+}
+
+int binarySearch(int A[], int l, int r, int x)
+{
+	if(r >= l)
+	{
+		int mid = l + (r-1)/2;
+
+		//If x lies in the middle of the sorted array.
+		if(A[mid] == x)
+			return mid;
+		//If x lies in the right side of the sorted array.
+		if(x > A[mid])
+			return binarySearch(A, mid+1, r, x);
+		//If x lies in the left side of the sorted array.
+		return binarySearch(A, l, mid-1, x);
+	}
+	return -1;
 }
