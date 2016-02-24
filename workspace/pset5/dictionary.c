688c1740bb77adabc6992fb53ca5292d83e9a24e
Kunal Das <kunaldasece@gmail.com>
2016-02-24 07:58:13 +0000

    working on code to check the spelling of the word

--- workspace/pset5/dictionary.c
+++ workspace/pset5/dictionary.c
@@ -15,13 +15,62 @@
 #include "dictionary.h"
 
 int wordcount = 0;
+node *root;
 /**
  * Returns true if word is in dictionary else false.
  */
 bool check(const char* word)
 {
-    // TODO
-    return false;
+    //Initializing the current head to the root of the TRIE.
+    node *current_head = root;
+    //Defining and allocating space to a variable to hold the character.
+    int ch = 0;   
+    for(int i = 0; word[i] != '\0'; i++)
+    {
+        ch = word[i];
+        //check if the character retrieved is an apostrophe.
+        if(ch == '\'')
+        {
+            //check if the 26th child denoting apostrophe is pointing to another character or not.
+            if(current_head->children[26] == NULL)
+            {
+                return false;
+            }
+            else
+            {
+                current_head = current_head->children[26];
+            }
+        }
+        else if(isalpha(ch))
+        {
+            //Checking if the character is in upper case or not.
+            if(isupper(ch))
+            {
+                ch = tolower(ch);
+            }
+            //determined the index of the child according to the alphabet.
+            ch = 'a' - ch;
+            
+            if(current_head->children[ch] == NULL)
+            {
+                return false;
+            }
+            else
+            {
+               current_head = current_head->children[ch]; 
+            }
+        }
+        
+    } //for loop ends and we have reached the end of the word.
+    //Checking if the bool value is set for the word or not.
+    if(current_head->is_word == true)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 /**
@@ -34,8 +83,7 @@ bool load(const char* dictionary)
     {
         return false;
     }
-    
-    node *root = malloc(sizeof(node));
+    root = malloc(sizeof(node));
     node *current_head = root;
     char *word = malloc(sizeof(char)*45);
     FILE *dictionaryFile = fopen(dictionary, "r");
