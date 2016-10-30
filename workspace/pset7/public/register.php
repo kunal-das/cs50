a632f1ac512b3928d27e99c6b76e2d8d1d8eb5f4
Kunal Das <kunaldasece@gmail.com>
2016-10-30 15:57:51 +0000

    Pset7 - Register Complete

---
+++ workspace/pset7/public/register.php
@@ -0,0 +1,57 @@
+<?php
+
+    // configuration
+    require("../includes/config.php"); 
+
+    // if user reached page via GET (as by clicking a link or via redirect)
+    if ($_SERVER["REQUEST_METHOD"] == "GET")
+    {
+        // else render form
+        render("register_form.php", ["title" => "Register"]);
+    }
+
+    // else if user reached page via POST (as by submitting a form via POST)
+    else if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        // validate submission
+        if (empty($_POST["username"]))
+        {
+            apologize("You must provide your username.");
+        }
+        else if (empty($_POST["password"]))
+        {
+            apologize("You must provide your password.");
+        }
+        else if(empty($_POST["confirmation"]))
+        {
+            apologize("You must retype your password");
+        }
+        else if($_POST["password"] != $_POST["confirmation"])
+        {
+            apologize("Password Mismatch !!...You must type the same password in both the password fields");
+        }
+
+        // query database for user
+        $rows = CS50::query("INSERT IGNORE INTO users (username, hash, cash) VALUES(?, ?, 10000.00)", $_POST["username"], password_hash($_POST["password"], PASSWORD_DEFAULT));
+        
+        // Check if the insertion worked correctly, only 1 row should be added and 1 should be returned.
+        if ($rows == 1)
+        {
+            
+            $rows = CS50::query("SELECT LAST_INSERT_ID() AS id");
+            $ID = $rows[0]["id"];
+            $_SESSION["id"] = $ID;
+            //call render to show "Successfull registration" page to the user.   
+            render("register_success.php", ["title" => "Registration Successfull"]);    
+        }
+        else if ($rows == false)
+        {
+            apologize("Username already exists !!...Please enter some other username.");
+        }
+        else
+        {
+            apologize("Sorry...An unknown error occurred");
+        }
+    }
+
+?>
---
+++ workspace/pset7/views/register_success.php
@@ -0,0 +1,10 @@
+<div><p style="text-align:center">REGISTRATION SUCCESSFULL</p></div>
+<form action="/" method="get">
+    <fieldset>
+       <div class="form-group">
+           <button class="btn btn-primary" type="submit">
+                Continue
+            </button>
+       </div> 
+    </fieldset>
+</form>
