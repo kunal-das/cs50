621acaabf23b047e53ba4b3165ea3cadb75c27a8
Kunal Das <kunaldasece@gmail.com>
2016-11-14 14:51:56 +0000

    Feature: Change Password -- Complete

---
+++ workspace/pset7/views/password_change_form.php
@@ -0,0 +1,22 @@
+<form action="change_password.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input class="form-control" name="current_password" placeholder="Current Password" type="password"/>
+        </div>
+        <div class="form-group">
+            <input class="form-control" name="new_password" placeholder="Re-type password" type="password"/>
+        </div>
+        <div class="form-group">
+            <input class="form-control" name="confirm_password" placeholder="Password" type="password"/>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-default" type="submit">
+                <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
+               Change Password 
+            </button>
+        </div>
+    </fieldset>
+</form>
+<div>
+    or go back to <a href="portfolio.php">Home</a>.
+</div>
